15f8f288650a3db7b3f4e5a82ba42c12998d5ac1
labor <labor@ilingyou.com>
2016-04-23 16:04:18 +0800

    自动生成协议

---
+++ AutoGenProtocol/AutoGenProtocol/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace AutoGenProtocol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument xml = new XmlDocument();
+            xml.Load("D:\\liubo\\github2\\test2\\AutoGenProtocol\\test.xml");
+
+            XmlNode env = xml.SelectSingleNode("env");
+            if (env != null)
+            {
+                Console.WriteLine("env-log = " + env.Attributes["log"].Value);
+            }
+
+            Console.WriteLine("root-name = " + xml.DocumentElement.Name);
+            
+        }
+    }
+}
