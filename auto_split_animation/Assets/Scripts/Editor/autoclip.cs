2709bad99dbe93a759f8d165baaee4be75a6215b
labor-home <badforlabor@gmail.com>
2016-04-29 21:51:02 +0800

    123

---
+++ auto_split_animation/Assets/Scripts/Editor/autoclip.cs
@@ -0,0 +1,313 @@
+/****************************************************************************
+Copyright (c) 2013-2014,Dalian-LingYOU tech.
+ This is not a free-ware .DO NOT use it without any authorization.
+ * 
+ * date : 2016/4/18 10:37:03
+ * author : Labor
+ * purpose : 自动切割动画
+****************************************************************************/
+using UnityEngine;
+using UnityEditor;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using NPOI.HSSF.UserModel;
+
+namespace MLGame
+{
+    static class SerializedPropertyExtension
+    {
+        public static void SetStringValue(this SerializedProperty self, string propertyName, string value)
+        {
+            self.FindPropertyRelative(propertyName).stringValue = value;
+        }
+    }
+
+
+    class AnimationAutoClip
+    {
+        [MenuItem("liubo/自动切割骨骼动画")]
+        public static void AutoClip()
+        {
+            string path = Selection.activeObject == null ? "" : AssetDatabase.GetAssetPath(Selection.activeObject);
+            Debug.Log("[切割动画] path=" + path);
+
+            ModelImporter mi = AssetImporter.GetAtPath(path) as ModelImporter;
+            if (mi == null)
+            {
+                Debug.Log("[切割动画] 没找到FBX文件！ path=" + path);
+            }
+            else
+            {
+                Debug.Log("[切割动画] 开始切割：path=" + path);
+
+                // 注意保留就动画的一些数据，比如绑定的event
+
+                List<ModelImporterClipAnimation> oldClips = new List<ModelImporterClipAnimation>();
+                List<ModelImporterClipAnimation> newClips = new List<ModelImporterClipAnimation>();
+                if (mi.clipAnimations != null)
+                {
+                    oldClips.AddRange(mi.clipAnimations);
+                }
+
+#if true//TEST
+                //AnimationUtility.GetAnimationClipSettings(null);
+                ModelImporterClipAnimation test = new ModelImporterClipAnimation();
+                test.name = "test";
+                test.takeName = "auto";
+                test.firstFrame = 0;
+                test.lastFrame = 30;
+                test.wrapMode = WrapMode.Default;
+                test.keepOriginalPositionY = true;
+                test.loop = false;
+                test.loopTime = false;
+                test.loopPose = false;
+                test.keepOriginalOrientation = false;
+                test.keepOriginalPositionY = false;
+                test.keepOriginalPositionXZ = false;
+                test.cycleOffset = 0;
+                test.heightOffset = 0;
+                test.rotationOffset = 0;
+
+                newClips.Add(test);
+                newClips.AddRange(oldClips);
+#endif
+
+                mi.clipAnimations = newClips.ToArray();
+                EditorUtility.SetDirty(Selection.activeObject);
+
+
+            }
+        }
+
+        [MenuItem("liubo/自动切割骨骼动画2")]
+        public static void AutoClip2()
+        {
+            /*
+             *  读取excel表，获取动画的信息：名字，起始帧，结束帧
+             *  再读取fbx的动画信息，对名字相同的进行信息merge
+             *      merge：起始帧，结束帧     
+             *      保留：event信息，mask信息，curve信息
+             *      如果有新加的，那么加入新的信息
+             * **/
+
+            // 读取excel
+            Dictionary<string, ConfigFile> excels = ReadXML(Application.dataPath + "/Art/animations.xls");
+            foreach (var it in excels)
+            {
+                Debug.Log("处理：" + it.Key);
+                ConfigFile csv = it.Value;
+                string[] assets = AssetDatabase.FindAssets(it.Key, new string[] {"Assets/Art"});
+                if (assets.Length > 0)
+                {
+                    string asset = AssetDatabase.GUIDToAssetPath(assets[0]);
+                    ModelImporter modelImporter = (ModelImporter)AssetImporter.GetAtPath(asset);
+                    SerializedObject serializedObject = modelImporter == null ? null : new SerializedObject(modelImporter);
+                    HashSet<string> processed_clip = new HashSet<string>();
+                    if (serializedObject == null)
+                    {
+                        Debug.Log("无法处理该文件：" + asset);
+                    }
+                    else
+                    { 
+                        SerializedProperty m_ClipAnimations = serializedObject.FindProperty("m_ClipAnimations");
+                        for (int i = 0; i < m_ClipAnimations.arraySize; i++)
+                        {
+                            SerializedProperty sp = m_ClipAnimations.GetArrayElementAtIndex(i);
+                            string clip_name = sp.FindPropertyRelative("name").stringValue;
+                            if (clip_name.Contains("@"))
+                            {
+                                clip_name = clip_name.Substring(clip_name.LastIndexOf("@") + 1);
+                            }
+                            ConfigLine csv_line = csv.FindData(clip_name);
+                            if (csv_line != null)
+                            {
+                                processed_clip.Add(clip_name);
+
+                                // 处理
+                                sp.FindPropertyRelative("firstFrame").floatValue = csv_line.GetColumn<float>("firstFrame");
+                                sp.FindPropertyRelative("lastFrame").floatValue = csv_line.GetColumn<float>("lastFrame");
+                            }
+                        }
+
+                        // 把剩下没处理的内容，作为新的动画，分割到fbx中
+                        Dictionary<string, ConfigLine> csv_data = csv.GetLines();
+                        foreach (var kt in csv_data)
+                        {
+                            if (processed_clip.Contains(kt.Key))
+                            {
+                                continue;
+                            }
+                            processed_clip.Add(kt.Key);
+                            ConfigLine csv_line = kt.Value;
+
+                            m_ClipAnimations.InsertArrayElementAtIndex(m_ClipAnimations.arraySize);
+                            SerializedProperty sp = m_ClipAnimations.GetArrayElementAtIndex(m_ClipAnimations.arraySize - 1);
+
+                            sp.FindPropertyRelative("name").stringValue = it.Key + "@" + kt.Key;
+                            sp.FindPropertyRelative("firstFrame").floatValue = csv_line.GetColumn<float>("firstFrame");
+                            sp.FindPropertyRelative("lastFrame").floatValue = csv_line.GetColumn<float>("lastFrame");
+                        }
+
+                        serializedObject.ApplyModifiedProperties();
+                        AssetDatabase.WriteImportSettingsIfDirty(asset);
+                    }
+
+                }
+            }
+
+
+        }
+        [MenuItem("liubo/切割选中的动画3")]
+        public static void AutoClip3()
+        {
+
+
+            // 获取m_AnimationClip，然后处理
+
+            ModelImporter modelImporter = Selection.activeObject == null ? null : (ModelImporter)AssetImporter.GetAtPath(AssetDatabase.GetAssetPath(Selection.activeObject));
+            SerializedObject serializedObject = modelImporter == null ? null : new SerializedObject(modelImporter);
+            //if (serializedObject != null)
+            //{
+            //    Debug.Log("[切割动画] 切割对象：" + serializedObject.targetObject.name);
+            //    SerializedProperty attr = serializedObject.GetIterator();
+            //    attr.Next(true);
+            //    do
+            //    {
+            //        Debug.Log("[切割动画] attr-name=" + attr.name);
+            //    }
+            //    while (attr.Next(true));
+            //}
+            SerializedProperty m_ClipAnimations = serializedObject == null ? null : serializedObject.FindProperty("m_ClipAnimations");
+            if (m_ClipAnimations != null)
+            {
+                if (m_ClipAnimations.arraySize > 0)
+                {
+                    //AnimationClip ac = m_ClipAnimations.GetArrayElementAtIndex(0).serializedObject.targetObject as AnimationClip;
+                    //Debug.Log("[切割动画] ac=" + (ac == null ? "none" : ac.name) + ", ref-obj=" + m_ClipAnimations.GetArrayElementAtIndex(0).serializedObject.targetObject.name);
+
+                    // 这个返回的是 ModelImporterClipAnimation 对象
+                    SerializedProperty sp = m_ClipAnimations.GetArrayElementAtIndex(0);
+
+                    //ShowProperties(sp);
+                    Debug.Log("[切割动画] ac.name=" + sp.FindPropertyRelative("name").stringValue);
+
+                    // 更改他的信息
+                    // 修改event
+                    {
+                        sp.FindPropertyRelative("firstFrame").floatValue = 10;
+                        sp.FindPropertyRelative("lastFrame").floatValue = 20;
+                    }
+
+
+                    SerializedProperty sp_event = sp.FindPropertyRelative("events");
+                    if (sp_event != null && sp_event.arraySize > 0)
+                    {
+                        Debug.Log("[切割动画] sp_event.arraySize=" + sp_event.arraySize);
+
+                        // 修改event
+                        if (false)
+                        {
+                            sp_event.InsertArrayElementAtIndex(sp_event.arraySize);
+                            SerializedProperty sp_one_event = sp_event.GetArrayElementAtIndex(sp_event.arraySize - 1);
+                            sp_one_event.SetStringValue("functionName", "123");
+                        }
+                    }
+                    serializedObject.ApplyModifiedProperties();
+                    AssetDatabase.WriteImportSettingsIfDirty(AssetDatabase.GetAssetPath(Selection.activeObject));
+                }
+                EditorUtility.SetDirty(Selection.activeObject);
+                AssetDatabase.SaveAssets();
+            }
+
+
+        }
+
+
+        static void ShowProperties(SerializedProperty sp)
+        {
+            SerializedProperty backup = sp.Copy();
+
+            {
+                sp = backup.Copy();
+                Debug.Log("[显示属性] sp.name=" + sp.displayName + ", " + sp.name);
+                sp.Next(true);
+                do
+                {
+                    Debug.Log("[显示属性] sp.name=" + sp.displayName + ", " + sp.name);
+                } while (sp.Next(false));
+            }
+        }
+
+        static Dictionary<string, ConfigFile> ReadXML(string path)
+        {
+            string shortName = Path.GetFileNameWithoutExtension(path);
+            //path = Application.dataPath + "/../Builder/builder_config.xls";
+
+            Dictionary<string, ConfigFile> ret = new Dictionary<string, ConfigFile>();
+
+            // !< 说明:客户端的表读取,就不走服务器那样的复杂处理啦..
+            HSSFWorkbook wk = new HSSFWorkbook(File.OpenRead(path));
+            HSSFSheet sheet = null;
+            string sheetname = "";
+            for (int a = 0; a < wk.NumberOfSheets; ++a)
+            {
+                sheet = wk.GetSheetAt(a) as HSSFSheet;
+                sheetname = wk.GetSheetName(a);
+                if (sheet == null)
+                {
+                    Debug.LogError("异常!" + shortName);
+                    continue;
+                }
+                if (ret.ContainsKey(sheetname))
+                {
+                    Debug.LogError("该excel表的sheet名字重复了：" + wk.GetSheetName(a));
+                }
+                else
+                {
+                    ConfigFile result = new ConfigFile(shortName + "_" + sheetname);
+                    List<String> header = new List<string>();
+                    for (int i = sheet.FirstRowNum; i <= sheet.LastRowNum; ++i)
+                    {
+                        HSSFRow row = sheet.GetRow(i) as HSSFRow;
+                        if (row == null) continue;
+                        ArrayList line = new ArrayList();
+                        for (int j = row.FirstCellNum; j <= row.LastCellNum; ++j)
+                        {
+                            HSSFCell cell = row.GetCell(j) as HSSFCell;
+                            if (cell == null) continue;
+                            if (i == sheet.FirstRowNum)
+                            {
+                                header.Add(cell.ToString());
+                            }
+                            else
+                            {
+                                line.Add(cell.ToString());
+                            }
+                        }
+                        if (i == sheet.FirstRowNum)
+                        {
+                            result.SetTitles(header.ToArray());
+                        }
+                        else
+                        {
+                            if (line.Count > 0)
+                            {
+                                result.AddData(line[0] as String, line);
+                            }
+                            else
+                            {
+                                // !< 读完了?
+                                break;
+                            }
+                        }
+                    }
+                    ret.Add(sheetname, result);
+                }
+            }
+
+            return ret;
+        }
+    }
+}
---
+++ auto_split_animation/Assets/Scripts/MLConfigLoader.cs
@@ -0,0 +1,267 @@
+﻿using UnityEngine;
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections;
+using System.Collections.Generic;
+
+// !< Discuss:
+// !< 1.为何不直接导出C#代码(类似lua那样),省去读取csv的时间?
+// !<   A:因为那样更新csv表格在理论上不可行了
+// !< 2.当前有几种读取表格方式?
+// !<   A:一种方式:使用MLConfigLoader进行加载,自动生成的数据结构只是减少了载入数据结构的重复性劳动.
+// !< 3.TODO
+// !< ->如何更好的省时省力???
+namespace MLGame
+{
+    public class ConfigLine
+    {
+        // !< parent
+        protected ConfigFile mFile;
+        // !< 数据行ID
+        protected string mKey;
+        // !< 数据行
+        protected ArrayList mDatas;
+
+        public string Key
+        {
+            get
+            {
+                return mKey;
+            }
+        }
+
+        public ConfigLine(ConfigFile file, string key, ArrayList data)
+        {
+            mFile = file;
+            mKey = key;
+            mDatas = data;
+        }
+
+        public T GetColumn<T>(string column)
+        {
+            int index = mFile.GetTitleIdx(column);
+            if (index < 0 || index >= mDatas.Count)
+            {
+                // !< WARN:不能使用LogError()否则程序直接终止
+                throw new Exception("[load config:" + mFile.FileName + "] error, column \'" + column + "\' doesn't exist! idx=" + index);
+            }
+            return (T)Convert.ChangeType(mDatas[index], typeof(T));
+        }
+
+        public T GetColumn<T>(int index)
+        {
+            if (index < 0 || index >= mDatas.Count)
+            {
+                // !< WARN:不能使用LogError()否则程序直接终止
+                throw new Exception("[load config:" + mFile.FileName + "] error, index \'" + index + "\' out of range! idx=" + index);
+            }
+            return (T)Convert.ChangeType(mDatas[index], typeof(T));
+        }
+
+        public int ColumnCount()
+        {
+            return mDatas.Count;
+        }
+    }
+
+    // 文件载入内存初始化对象后,是不是就可以删除了?
+    public class ConfigFile
+    {
+        // csv文件名
+        protected string mFileName;
+        // csv标题,用于查找时快速找到索引
+        protected Dictionary<string, int> mTitleIndexs;
+        // csv数据部分,用于查找内容
+        protected Dictionary<string, ConfigLine> mDatas;
+        protected Dictionary<string, ArrayList> mDataLists;
+
+        public string FileName
+        {
+            get
+            {
+                return mFileName;
+            }
+        }
+        public ConfigFile(string file)
+        {
+            mFileName = file;
+            mTitleIndexs = new Dictionary<string, int>();
+            mDataLists = new Dictionary<string, ArrayList>();
+            mDatas = new Dictionary<string, ConfigLine>();
+        }
+
+        public void Reset()
+        {
+            mTitleIndexs.Clear();
+            mDataLists.Clear();
+            mDatas.Clear();
+        }
+
+        public void SetTitles(string[] titles)
+        {
+            for (int i = 0; i < titles.Length; ++i )
+            {
+                mTitleIndexs.Add(titles[i], i);
+            }
+        }
+
+        public int GetTitleIdx(string title)
+        {
+            int idx = -1;
+            if (mTitleIndexs.TryGetValue(title,out idx))
+            {
+                return idx;
+            }
+            return -1;
+        }
+
+        public void AddData(string key, ArrayList data)
+        {
+            if (mDatas.ContainsKey(key))
+            {
+                throw new Exception(String.Format("表{0}加载失败, ID重了 = {1}", mFileName, key));
+            }
+            else
+            {
+                mDatas.Add(key, new ConfigLine(this, key, data));
+                mDataLists.Add(key, data);
+            }
+        }
+
+        public T FindData<T>(string key, string colum)
+        {
+            ConfigLine line;
+            if (!mDatas.TryGetValue(key, out line))
+            {
+                throw new Exception("[load config:" + FileName + "] error, data \'" + key + "\' doesn't exit at all!");
+            }
+
+            return line.GetColumn<T>(colum);
+        }
+
+        public ConfigLine FindData(string key)
+        {
+            ConfigLine line;
+            if (!mDatas.TryGetValue(key, out line))
+            {
+                return null;
+            }
+            return line;
+        }
+
+        // !< 返回的是ConifigLine的列表
+        public Dictionary<string, ConfigLine> GetLines()
+        {
+            return mDatas;
+        }
+
+        [Obsolete("this function was deprecated, use GetLines() instead!")]
+        public Dictionary<string, ArrayList> GetData()
+        {
+            return mDataLists;
+        }
+    }
+
+    // !< WARN:当前读取CSV表走的是字符串处理,效率不咋高,同时,行内的内容拆分走的是正则表达式.
+    // !<   未来优化时,可以考虑将所有表格序列化为txt再读取!
+    // !< WARN:当前解析CSV一行时,是拆分为一个一个字段(也可以走序列化)后,再进行类型转化并传递给对应的成员变量赋值!!
+    // !<   未来优化时,可以改为运行时再进行类型转换!!!(或者也可以考虑使用序列化解决)
+    public class MLConfigLoader
+    {
+        private static Dictionary<string, ConfigFile> mConfigFiles = new Dictionary<string, ConfigFile>();
+        private static Regex mCSVRegex = new Regex(@"(((?<x>(?=[,\r\n]+))|""(?<x>([^""]|"""")+)""|(?<x>[^,\r\n]+)),?)", RegexOptions.ExplicitCapture);
+        public static float parseTime = 0f;
+        public static float readTime = 0f;
+        public static string[] SplitSingleLine(string line)
+        {
+            return (from Match m in mCSVRegex.Matches(line) select m.Groups[1].Value).ToArray();
+        }
+
+        public static ConfigFile LoadSingleConfigure(string filename)
+        {
+            ConfigFile file = null;
+
+            float delta = Time.realtimeSinceStartup;
+            TextAsset config = null;//ResourceLoad.LoadAsset<TextAsset>("Config/" + filename);
+            delta = Time.realtimeSinceStartup - delta;
+            readTime += delta;
+            if (config != null)
+            {
+                file = LoadSingleConfigure(filename, config.text);
+            }
+
+            return file;
+        }
+
+        public static ConfigFile LoadSingleConfigure(string filename, string data)
+        {
+            // !< WARN : 由于是字符处理方式,需要保证输入的data为UTF-8格式
+            // 拆分为行
+            string[] lineArray = data.Split("\n"[0]);
+
+            if (lineArray.Length < 1)
+            {
+                // error
+                return null;
+            }
+            ConfigFile fileData = new ConfigFile(filename);
+
+            // 第一行为csv头
+            float delta = Time.realtimeSinceStartup;
+            string[] header = SplitSingleLine(lineArray[0]);
+            fileData.SetTitles(header);
+
+            for (int i = 1; i < lineArray.Length;++i )
+            {
+                if (lineArray[i] != "")
+                {
+                    string[] line = SplitSingleLine(lineArray[i]);
+                    ArrayList dataLine = ArrayList.Adapter(line);
+                    fileData.AddData(line[0], dataLine);
+                }
+            }
+
+            delta = Time.realtimeSinceStartup - delta;
+            parseTime += delta;
+
+            return fileData;
+        }
+
+        //不建议对外使用，未来方便统一清理掉config的引用
+        private static ConfigFile GetConfigure(string filename)
+        {
+            ConfigFile result = null;
+            if (!mConfigFiles.TryGetValue(filename, out result))
+            {
+                result = LoadSingleConfigure(filename);
+                mConfigFiles.Add(filename, result);
+            }
+
+            return result;
+        }
+
+        public static string FindConfigData(string filename, int key, string column)
+        {
+            return FindConfigData<string>(filename, key.ToString(), column);
+        }
+
+        public static T FindConfigData<T>(string filename, string key, string column)
+        {
+            ConfigFile result = GetConfigure(filename);
+            if (result == null)
+                return default(T);
+
+            return result.FindData<T>(key, column);
+        }
+
+        public static T FindConfigData<T>(string filename, int key, string column)
+        {
+            ConfigFile result = GetConfigure(filename);
+            if (result == null)
+                return default(T);
+
+            return result.FindData<T>(key.ToString(), column);
+        }
+    }
+}
