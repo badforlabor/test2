fefbf9e5f89e86814338f7267d14e35b2c59f444
bad-labor <bad-labor@home.com>
2016-06-28 20:25:04 +0800

    局域网

---
+++ test_lan/Assets/ClientMono.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Text;
+
+public class ClientMono : MonoBehaviour {
+
+    public int Port = 13131;
+
+	// Use this for initialization
+	void Start () {
+        var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        socket.Connect("localhost", Port);
+        socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveMessage), socket);
+	}
+    void ReceiveMessage(IAsyncResult ar)
+    {
+        try
+        {
+            var socket = ar.AsyncState as Socket;
+
+            //方法参考：http://msdn.microsoft.com/zh-cn/library/system.net.sockets.socket.endreceive.aspx
+            var length = socket.EndReceive(ar);
+            //读取出来消息内容
+            var message = Encoding.Unicode.GetString(buffer, 0, length);
+            //显示消息
+            //Console.WriteLine(message);
+
+            //接收下一个消息(因为这是一个递归的调用，所以这样就可以一直接收消息了）
+            socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveMessage), socket);
+        }
+        catch (Exception ex)
+        {
+            //Console.WriteLine(ex.Message);
+        }
+    
+    }
+    byte[] buffer = new byte[1024];
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ test_lan/Assets/ServerMono.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Text;
+
+public class ServerMono : MonoBehaviour {
+
+    // http://www.cnblogs.com/chenxizhang/archive/2011/09/10/2172994.html
+
+    string ServerIP = "";
+    int ServerPoint = 13131;
+    Socket ServerSocket = null;
+
+	// Use this for initialization
+	void Start () {
+
+        // 启动服务器
+        IPAddress[] ipaddress = Dns.GetHostAddresses(Dns.GetHostName());
+        ServerIP = ipaddress[0].ToString();
+        Debug.Log("server ip=" + ServerIP);
+
+        ServerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        ServerSocket.Bind(new IPEndPoint(ipaddress[0], ServerPoint));
+        ServerSocket.Listen(4);
+
+        ServerSocket.BeginAccept(new AsyncCallback(ClientAccepted), ServerSocket);
+
+	}
+    void ClientAccepted(IAsyncResult ar)
+    {
+        var socket = ar.AsyncState as Socket;
+        var client = socket.EndAccept(ar);
+
+        client.Send(System.Text.Encoding.UTF8.GetBytes("你好，客户端"));
+
+        client.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveMessage), client);
+    }
+    byte[] buffer = new byte[1024];
+    void ReceiveMessage(IAsyncResult ar)
+    {
+        var client = ar.AsyncState as Socket;
+        var length = client.EndReceive(ar);
+        var message = System.Text.Encoding.UTF8.GetString(buffer, 0, length);
+    }
+    
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
