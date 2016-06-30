b78edaea9658a2bff3f0985c507ab13e9f0c56a4
labor <labor@ilingyou.com>
2016-06-30 17:45:34 +0800

    测试双splash

---
+++ test_splash/Assets/nextlevel.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class nextlevel : MonoBehaviour {
+
+	public float LevelTime = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(LevelTime > 0)
+		{
+			LevelTime -= Time.deltaTime;	
+			if(LevelTime <= 0)
+			{				
+                Application.LoadLevel(1);
+			}
+		}		
+	}
+}
